92d5e1479dfdbce950c2ec07038dff401920db89
Marini34 <160694080+Marini34@users.noreply.github.com>
2024-12-01 04:15:43 +0700

    test getenv or _env from json or .env

--- koneksi.php
+++ koneksi.php
@@ -8,23 +8,27 @@ if (getenv('VERCEL_ENV') === 'production' || getenv('VERCEL_ENV') === 'preview')
 
    define('BASEURL', $fullUrl); // untuk production
 
-   $host = $_ENV['PG_HOST'] ?? "ep-gentle-smoke-a40kybs6.us-east-1.aws.neon.tech";
-   $port = $_ENV['PG_PORT'] ?? "5432";
-   $db = $_ENV['PG_DB'] ?? "verceldb";
+   $host = $_ENV['PG_HOST'] ?? getenv('PG_HOST');
+   $port = $_ENV['PG_PORT'] ?? getenv('PG_PORT');
+   $db = $_ENV['PG_DB'] ?? getenv('PG_DB');
+   $user = $_ENV['PG_USER'] ?? getenv('PG_USER');
+   $password = $_ENV['PG_PASSWORD'] ?? getenv('PG_PASSWORD');
+   $endpoint = $_ENV['PG_ENDPOINT'] ?? getenv('PG_ENDPOINT');
+   $DSN = "pgsql:host=$host;port=$port;dbname=$db;sslmode=require;options=endpoint=$endpoint";
    //pgsql:host=ep-frosty-haze-a1kda0nu.ap-southeast-1.aws.neon.tech;port=5432;dbname=neondb;sslmode=require;options=endpoint=ep-frosty-haze-a1kda0nu
    // postgres://default:ZtnC4Dg5lLzI@ep-gentle-smoke-a40kybs6.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require
-   define('DSN', "pgsql:host=ep-gentle-smoke-a40kybs6.us-east-1.aws.neon.tech;port=5432;dbname=verceldb;sslmode=require;options=endpoint=ep-gentle-smoke-a40kybs6");
-   define('DB_USER', $_ENV['PG_USER'] ?? "default");
-   define('DB_PASS', $_ENV['PG_PASSWORD'] ?? "ZtnC4Dg5lLzI");
-   define('DB_ENDPOINT', $_ENV['PG_ENDPOINT'] ?? "ep-gentle-smoke-a40kybs6");
+   define('DSN', $_ENV['PG_DSN'] ?? getenv('PG_DSN'));
+   define('DB_USER', $user);
+   define('DB_PASS', $password);
+   define('DB_ENDPOINT', $endpoint);
 
-   echo "<script>console.log('getenv: ". getenv('PG_HOST') ."');</script>";
-   echo "<script>console.log('_ENV: ". $_ENV['PG_HOST'] ."');</script>";
+   echo "<script>console.log('getenv: ". getenv('SOME') ."');</script>";
+   echo "<script>console.log('verceljson or .env >getenv getenv: ". getenv('GETENV') ."');</script>";
+   echo "<script>console.log('verceljson or .env >env getenv: ". getenv('ENV') ."');</script>";
+   echo "<script>console.log('env _ENV: ". $_ENV['SOME'] ."');</script>";
+   echo "<script>console.log('verceljson or .env >getenv _ENV: ". $_ENV['GETENV'] ."');</script>";
+   echo "<script>console.log('verceljson or .env >env _ENV: ". $_ENV['ENV'] ."');</script>";
 } else {
-   // echo "<script>
-   //       alert('Database mySQL');
-   //   </script>";
-   define('DB_NOW_MYSQL', true);
    // sesuaikan nama url !!! hati-hati terhadap konfigurasi route ke server
    define('BASEURL', 'http://localhost:8080/ma/sig-lanjut'); // kita arahkan ke halaman public
    // var_dump($host, $port, $db, DB_USER, DB_PASS);
